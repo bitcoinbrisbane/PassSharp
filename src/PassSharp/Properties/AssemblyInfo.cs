2768d12e0d8e445e9dd7bcf4fb5761a6d4436819
Jason <jason@scurker.com>
2016-11-02 09:50:52 -0500

    nuget settings

--- src/PassSharp/Properties/AssemblyInfo.cs
+++ src/PassSharp/Properties/AssemblyInfo.cs
@@ -1,13 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Information about this assembly is defined by the following attributes. 
+// Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle ("PassSharp")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("An Apple Wallet Passbook Library in C#/.NET")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
+[assembly: AssemblyCompany ("Daxko")]
 [assembly: AssemblyProduct ("")]
 [assembly: AssemblyCopyright ("jwilson")]
 [assembly: AssemblyTrademark ("")]
@@ -19,7 +19,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion ("1.0.*")]
 
-// The following attributes are used to specify the signing key for the assembly, 
+// The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
