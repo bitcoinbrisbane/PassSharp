c6a95db3a4f8b35744120bc510d3aea58a915e38
Jason <jason@scurker.com>
2016-11-02 10:57:42 -0500

    Add signature check

--- src/test/PassGenerationSpecs.cs
+++ src/test/PassGenerationSpecs.cs
@@ -6,27 +6,28 @@ using System.Security.Cryptography.X509Certificates;
 using System.IO;
 using System.Reflection;
 using ServiceStack.Text;
-using System.Collections;
+using System.IO.Compression;
+using System.Security.Cryptography.Pkcs;
 
 #pragma warning disable 414
 
 namespace Test
 {
 
-  public class PassWriterMock : PassWriter 
+  public class PassWriterMock : PassWriter
   {
 
-    public static Dictionary<string, string> GenerateManifest()
+    public static new Dictionary<string, string> GenerateManifest()
     {
       return PassWriter.GenerateManifest();
     }
 
-    public static byte[] GenerateSignature(byte[] bytes, X509Certificate2 appleCert, X509Certificate2 passCert)
+    public static new byte[] GenerateSignature(byte[] bytes, X509Certificate2 appleCert, X509Certificate2 passCert)
     {
       return PassWriter.GenerateSignature(bytes, appleCert, passCert);
     }
 
-    public static string ToJson(Pass pass)
+    public static new string ToJson(Pass pass)
     {
       return PassWriter.ToJson(pass);
     }
@@ -74,26 +75,31 @@ namespace Test
     //{
     //  static Dictionary<string, string> manifest;
 
-    //  Because of = () => {};
-
-    //  It should_have_manifest_entries = () => manifest.Keys.Count.ShouldEqual(5);
-    //  It should_not_contain_the_manifest = () => manifest.ContainsKey("manifest.json").ShouldBeFalse();
-    //}
-
-    //public class when_adding_a_signature
-    //{
-    //  static Dictionary<string, s
-
     //  Because of = () => {
     //    using (var stream = new MemoryStream()) {
-    //      PassWriter.WriteToStream(pass, stream, appleCert, passCert);
+    //      PassWriterMock.WriteToStream(pass, stream, appleCert, passCert);
+    //      manifest = PassWriterMock.GenerateManifest();
     //    }
     //  };
 
-
+    //  It should_have_manifest_entries = () => manifest.Keys.Count.ShouldEqual(2);
+    //  It should_not_contain_the_manifest = () => manifest.ContainsKey("manifest.json").ShouldBeFalse();
+    //  It should_have_correct_hash_for_manifest_entry = () => manifest["pass.json"].ShouldEqual("1715e0ac7a30b2173ad5b616f483015f5911c418");
     //}
 
+    public class when_generating_a_signature
+    {
+      static ZipArchive zip;
 
+      Because of = () => {
+        using (var stream = new MemoryStream()) {
+          PassWriter.WriteToStream(pass, stream, appleCert, passCert);
+          zip = new ZipArchive(stream);
+        }
+      };
+
+      It should_have_a_signature_entry = () => zip.GetEntry("signature").ShouldNotBeNull();
+    }
 
   }
 
